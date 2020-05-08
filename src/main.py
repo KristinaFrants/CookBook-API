867b36af8f0df2fb2a64f54a2b3f9e5af19e34f5
KristinaFrants <cristina.chiticari@gmail.com>
2020-05-08 16:49:09 +0000

    fixed cooctips column

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from models import db
-#from models import Person
+from models import Cooking
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -37,7 +37,69 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
-# this only runs if `$ python src/main.py` is executed
+
+@app.route('/recipes', methods=['POST','GET'])
+def get_recipes():
+    # get request
+    if request.method == 'GET':
+        all_recipe = Cooking.query.all()
+        all_recipe = list(map(lambda x : x.serialize(), all_recipe))
+        
+        return jsonify(all_recipe), 200
+
+    if request.method == 'POST':
+        body = request.get_json() 
+        if body is None:
+            raise APIException("Specify JSON body", status_code=400)
+        if "description" not in body:
+            raise APIException("Specify description", status_code=400)
+        if "email" not in body:
+            raise APIException("Specify Email", status_code=400)
+
+        recipes1 = Cooking(author = body['author'], email = body['email'], name = body['name'], description = body['description'], servings = body['servings'], cooktime = body['cooktime'], image = body['image'], cooktips = body['cooktips'], ingridients = body['ingridients'])
+        db.session.add(recipes1)
+        db.session.commit()
+
+
+    
+        return "ok", 200
+
+    return "invalid method", 404
+
+@app.route('/recipes/<int:cooking_id>', methods=['PUT','GET', 'DELETE'])
+def get_single_recipe(cooking_id):
+    #put request
+    if request.method == 'PUT':
+        body = request.get_json()
+        if body is None:
+            raise APIException("Specify JSON body", status_code=400)
+        
+        recipe1 = Cooking.query.get(cooking_id)
+        if "description" in body:
+            recipe1.description = body["description"]
+        db.session.commit()
+        
+        return jsonify(recipe1.serialize()), 200
+    
+    #get request
+    if request.method == "GET":
+        recipe1 = Cooking.query.get(cooking_id)
+        if recipe1 is None:
+            raise APIException("Recipe Not Found", status_code=404)
+        return jsonify(recipe1.serialize()), 200
+
+    #delete request
+    if request.method == 'DELETE':
+        recipe1 = Cooking.query.get(cooking_id)
+        if recipe1 is None:
+            raise APIException("Recipe Not Found", status_code=404)
+        db.session.delete(recipe1)
+        db.session.commit()
+        return "recipe deleted", 200
+
+    return "invalid method", 404
+    
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -2,16 +2,36 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# class Person(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
+class Cooking(db.Model):
+    __tablename__ = "cooking"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=False, nullable=False)
+    servings = db.Column(db.String(120))
+    ingridients = db.Column(db.String(4500))
+    cooktime = db.Column(db.String(16))
+    description = db.Column(db.String(4500))
+    author = db.Column(db.String(120), nullable=False)
+    cooktips = db.Column(db.String(4500))
+    image = db.Column(db.String(150), nullable=True)
+    # video = db.Column(db.String(150), nullable=True)
+    
 
-#     def __repr__(self):
-#         return '<Person %r>' % self.username
+    def __repr__(self):
+        return '<Cooking %r>' % self.recipe
 
-#     def serialize(self):
-#         return {
-#             "username": self.username,
-#             "email": self.email
-#         }
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "name": self.name,
+            "servings": self.servings,
+            "cooktime": self.cooktime,
+            "ingridients": self.ingridients,
+            "description": self.description,
+            "author": self.author,
+            "image" : self.image,
+            "cooktips" : self.cooktips,
+            # "alerts": list(map(lambda bubu : bubu.serialize(), self.alerts)),
+            # "pets": list(map(lambda x : x.serialize(), self.pets))
+        }
